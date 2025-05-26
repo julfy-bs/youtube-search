66f5b03d6016f8884224055c711e57472306d7f2
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2025-05-26 23:01:35 +0300

    feat: обновляет адрес для прод приложения.

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: process.env.NODE_ENV === 'production' ? '/youtube-search/' : '/',
 	plugins: [vue()],
 	resolve: {
 		alias: {
 			'@/': `${path.resolve(__dirname, 'src')}/`
 		}
-	}
+	},
 });
