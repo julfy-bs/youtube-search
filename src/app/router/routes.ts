811dcf765da9dc59d0d69fe0f7fe968d9cf6979a
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2025-05-27 08:51:30 +0300

    fix: исправляет ошибку роутинга на prod gh-pages.

--- src/app/router/routes.ts
+++ src/app/router/routes.ts
@@ -2,8 +2,16 @@ import DetailVideoPage from '@/pages/DetailVideoPage.vue';
 import FavouriteVideosPage from '@/pages/FavouriteVideosPage.vue';
 import SearchVideoPage from '@/pages/SearchVideoPage.vue';
 
+const BASE_URL = process.env.NODE_ENV === 'production'
+	? '/youtube-search/'
+	: '/';
+
 export const routes = [
-	{ path: '/', component: SearchVideoPage },
-	{ path: '/:videoId', name: 'detail', component: DetailVideoPage },
-	{ path: '/favourite', component: FavouriteVideosPage },
-]
+	{ path: `${ BASE_URL }`, component: SearchVideoPage },
+	{
+		path: `${ BASE_URL }:videoId`,
+		name: 'detail',
+		component: DetailVideoPage,
+	},
+	{ path: `${ BASE_URL }favourite`, component: FavouriteVideosPage },
+];
